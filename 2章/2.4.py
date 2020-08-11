99b908803ea0292dc589ec9331b196bbce252be3
Nattchi <Yuu.tan@icloud.com>
2020-08-12 06:24:33 +0900

    2章

---
+++ 2章/2.4.py
@@ -0,0 +1,16 @@
+﻿from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html, 'html.parser')
+images = bs.find_all('img', {'src': re.compile('..\/img\/gifts\/img.*.jpg')})
+trs = bs.find_all('tr', {'class' : re.compile('^gift$')})
+
+for image in images:
+    print(image['src'])
+    print(image)
+
+for tr in trs:
+    print(tr['id'])
+    print(tr['class'])
---
+++ 2章/2.6.py
@@ -0,0 +1,14 @@
+﻿from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html, 'html.parser')
+print("-----bs4-----")
+print(bs.find_all('', text='Or maybe he\'s only resting?'))
+print("-----lambda-----")
+print(bs.find_all(lambda tag : tag.get_text() == 'Or maybe he\'s only resting?'))
+double_attrs = bs.find_all(lambda tag: len(tag.attrs) == 2)
+
+for double_attr in double_attrs:
+    print(double_attr)
